9ba398e5da588e7e8cff40736f422f8fb83f4ac6
Riya-Sahu1212 <159940278+Riya-Sahu1212@users.noreply.github.com>
2024-11-11 15:52:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://job-seaking-website-tyb7.vercel.app/api/v1/user/getuser",
           {
             withCredentials: true,
           }
