71bead15f2e0c5d1c746640efa41619126446647
Shaurya <shauryapandey4136@gmail.com>
2024-12-08 16:58:42 +0530

    3

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const bcrypt = require('bcrypt');
